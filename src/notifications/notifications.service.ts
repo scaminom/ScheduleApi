259b0ac9d30804e55ce1b72b33eed14e5ffb4dbf
devjaes <merajair17@gmail.com>
2024-07-23 23:02:15 -0500

    chore: Update NotificationsService to decrypt subscription endpoint before sending push notification

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -2,10 +2,14 @@ import { Injectable, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import * as webpush from 'web-push'
 import { IEnvConfig } from '../config/config'
+import { CryptoService } from 'src/crypto/crypto.service'
 
 @Injectable()
 export class NotificationsService {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly cryptoService: CryptoService,
+  ) {
     webpush.setVapidDetails(
       this.configService.get<IEnvConfig>('config').VAPID_SUBJECT,
       this.configService.get<IEnvConfig>('config').VAPID_PUBLIC_KEY,
@@ -18,7 +22,13 @@ export class NotificationsService {
     data: any,
   ) {
     try {
-      await webpush.sendNotification(subscription, JSON.stringify(data))
+      const endpoint = await this.cryptoService.decryptString(
+        subscription.endpoint,
+      )
+      await webpush.sendNotification(
+        { ...subscription, endpoint },
+        JSON.stringify(data),
+      )
       Logger.log('Push notification sent successfully')
     } catch (error) {
       Logger.error('Error sending push notification', error)
