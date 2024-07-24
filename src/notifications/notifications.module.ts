4b42b3816fefbc9e4165814fad3076a810d503f5
devjaes <merajair17@gmail.com>
2024-07-23 23:03:18 -0500

    chore: Update NotificationsModule to import CryptoModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { NotificationsService } from './notifications.service'
+import { CryptoModule } from 'src/crypto/crypto.module'
 
 @Module({
   providers: [NotificationsService],
   exports: [NotificationsService],
+  imports: [CryptoModule],
 })
 export class NotificationsModule {}
