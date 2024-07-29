5d041e21fb72c8c6a67a7150a428cbb3b9659e80
devjaes <merajair17@gmail.com>
2024-07-28 22:38:23 -0500

    chore: Update appointments interface to include color field

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -3,10 +3,11 @@ import { JobsService } from './jobs.service'
 import { JobsController } from './jobs.controller'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { InspectionsModule } from 'src/inspections/inspections.module'
+import { JobsGateway } from './jobs.gateway';
 
 @Module({
   controllers: [JobsController],
   imports: [PrismaModule, InspectionsModule],
-  providers: [JobsService],
+  providers: [JobsService, JobsGateway],
 })
 export class JobsModule {}
