a532f91d2658ca012c1ffaea6a85f115411307ea
devjaes <merajair17@gmail.com>
2024-07-29 15:35:14 -0500

    chore: Add InspectionsGateway to InspectionsModule providers

--- src/inspections/inspections.module.ts
+++ src/inspections/inspections.module.ts
@@ -3,11 +3,12 @@ import { InspectionsService } from './inspections.service'
 import { InspectionsController } from './inspections.controller'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { AppointmentsModule } from 'src/appointments/appointments.module'
+import { InspectionsGateway } from './inspections.gateway'
 
 @Module({
   controllers: [InspectionsController],
   imports: [PrismaModule, forwardRef(() => AppointmentsModule)],
-  providers: [InspectionsService],
+  providers: [InspectionsService, InspectionsGateway],
   exports: [InspectionsService],
 })
 export class InspectionsModule {}
